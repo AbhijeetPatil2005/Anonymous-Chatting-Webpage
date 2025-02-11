b1d5183aa775356cf2f237213aaeef23fa16bb3e
AbhijeetPatil2005 <abhijeetpatil032005@gmail.com>
2025-02-11 12:15:47 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-from chat import app as flask_app
-from serverless_wsgi import handle_request
-
-def handler(event, context):
-    return handle_request(flask_app, event, context)
